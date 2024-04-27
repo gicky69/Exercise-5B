bf352dcd4d55682f3dc4d67aa6ec1a7fb248acc7
Gicky69 <carlandrewv@yahoo.com>
2024-04-27 12:49:31 +0800

    initial

---
+++ src/ControlPanel.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ControlPanel {
+    JPanel ControlPanel;
+    MapPanel MapPanel;
+    JButton AddNode;
+    JButton AddEdge;
+
+    public ControlPanel(MapPanel mapPanel){
+        this.MapPanel = mapPanel;
+        ControlPanel = new JPanel();
+        ControlPanel.setBounds(0, 720/2, 1280, 720/2);
+        ControlPanel.setBackground(Color.DARK_GRAY);
+
+        AddNode = new JButton("Add Node");
+        AddNode.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                MapPanel.MapPanel.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        MapPanel.addNode(e.getX(), e.getY());
+                    }
+                });
+            }
+        });
+
+        ControlPanel.add(AddNode);
+        ControlPanel.setVisible(true);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,33 @@
+
+import javax.naming.ldap.Control;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class Main {
+    JFrame MainFrame;
+    MapPanel MapPanel;
+    ControlPanel ControlPanel;
+
+
+    public Main(){
+        MainFrame = new JFrame("Exercise 5B");
+        MapPanel = new MapPanel();
+        ControlPanel = new ControlPanel(MapPanel);
+
+        MainFrame.setSize(1280, 720);
+        MainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        MainFrame.setLayout(null);
+
+        MainFrame.setVisible(true);
+        MainFrame.setLocationRelativeTo(null);
+
+        MainFrame.add(MapPanel.MapPanel);
+        MainFrame.add(ControlPanel.ControlPanel);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
