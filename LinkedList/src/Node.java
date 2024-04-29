85a9bb9320bbe5797a5c663693712cf82cb6c13c
Joaquin Pacete <jrpacete@student.apc.edu.ph>
2024-04-29 16:40:39 +0800

    test

---
+++ LinkedList/src/Node.java
@@ -0,0 +1,14 @@
+public class Node {
+    String data;
+    Node prev;
+    Node next;
+    int length;
+
+    public Node(String data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+        this.length = Integer.parseInt(null);
+
+    }
+}
