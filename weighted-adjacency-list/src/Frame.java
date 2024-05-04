3405c72329c64fcca066031027ed5f31ce433fb2
Joaquin Pacete <jrpacete@student.apc.edu.ph>
2024-05-04 21:17:25 +0800

    Add and Improve MapPanel & Frame

--- weighted-adjacency-list/src/Frame.java
+++ weighted-adjacency-list/src/Frame.java
@@ -1,17 +1,69 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Map;
 
 public class Frame {
-    MapPanel MapPanel;
+    MapPanel mapPanel;
+    JMenuBar menuBar;
+    JMenu menu;
+    JMenuItem addNode, connectNode, selectNode, deleteNode;
 
     public Frame() {
         JFrame frame = new JFrame("MAP");
-
-        MapPanel = new MapPanel();
+        frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1280, 720);
 
-        frame.add(MapPanel);
+        mapPanel = new MapPanel();
+        mapPanel.setBounds(0,0,1280,720);
+
+        // MENU BAR
+        menuBar = new JMenuBar();
+        menu = new JMenu("Options");
+        addNode = new JMenuItem("Add Node");
+        connectNode = new JMenuItem("Connect Node");
+        selectNode = new JMenuItem("Select Node");
+        deleteNode = new JMenuItem("Delete Node");
+        menu.add(addNode);
+        menu.add(connectNode);
+        menu.add(selectNode);
+        menu.add(deleteNode);
+        menuBar.add(menu);
+        frame.setJMenuBar(menuBar);
+
+        addNode.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mapPanel.setMode(MapPanel.Mode.ADD_NODE);
+            }
+        });
+
+        selectNode.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mapPanel.setMode(MapPanel.Mode.SELECT_NODE);
+            }
+        });
+
+        connectNode.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mapPanel.setMode(MapPanel.Mode.CONNECT_NODE);
+            }
+        });
+
+        deleteNode.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mapPanel.setMode(MapPanel.Mode.DELETE_NODE);
+            }
+        });
+
+        frame.add(mapPanel);
         frame.setVisible(true);
+
     }
+
 }
